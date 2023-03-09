9ddd105779e40291a28d073f736161f2dfdbd848
nitish shrivastav <nitishshrivastav2@gmail.com>
2023-03-09 22:30:10 +0530

    snakebite

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from home import views
+from django.urls import re_path as url
+
+urlpatterns =[
+     url('^$',views.homepage),
+     url('/predict',views.predict),
+     url('/about',views.about),
+     url('/firstaid',views.firstaid),
+     url('/professionaltreatment',views.pt),
+]
